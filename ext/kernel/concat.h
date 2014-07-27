d661b67a9d0d2a5e92eff8ea02fafdfcd6aa3946
HugoHiram <hugohiram@gmail.com>
2014-07-28 00:09:02 +0200

    concat updated

--- ext/kernel/concat.h
+++ ext/kernel/concat.h
@@ -1,8 +1,3 @@
-#define ZEPHIR_CONCAT_SS(result, op1, op2) \
-	 zephir_concat_ss(&result, op1, sizeof(op1)-1, op2, sizeof(op2)-1, 0 TSRMLS_CC);
-#define ZEPHIR_SCONCAT_SS(result, op1, op2) \
-	 zephir_concat_ss(&result, op1, sizeof(op1)-1, op2, sizeof(op2)-1, 1 TSRMLS_CC);
-
 #define ZEPHIR_CONCAT_SV(result, op1, op2) \
 	 zephir_concat_sv(&result, op1, sizeof(op1)-1, op2, 0 TSRMLS_CC);
 #define ZEPHIR_SCONCAT_SV(result, op1, op2) \
@@ -18,15 +13,20 @@
 #define ZEPHIR_SCONCAT_VS(result, op1, op2) \
 	 zephir_concat_vs(&result, op1, op2, sizeof(op2)-1, 1 TSRMLS_CC);
 
+#define ZEPHIR_CONCAT_VSVSV(result, op1, op2, op3, op4, op5) \
+	 zephir_concat_vsvsv(&result, op1, op2, sizeof(op2)-1, op3, op4, sizeof(op4)-1, op5, 0 TSRMLS_CC);
+#define ZEPHIR_SCONCAT_VSVSV(result, op1, op2, op3, op4, op5) \
+	 zephir_concat_vsvsv(&result, op1, op2, sizeof(op2)-1, op3, op4, sizeof(op4)-1, op5, 1 TSRMLS_CC);
+
 #define ZEPHIR_CONCAT_VV(result, op1, op2) \
 	 zephir_concat_vv(&result, op1, op2, 0 TSRMLS_CC);
 #define ZEPHIR_SCONCAT_VV(result, op1, op2) \
 	 zephir_concat_vv(&result, op1, op2, 1 TSRMLS_CC);
 
 
-void zephir_concat_ss(zval **result, const char *op1, zend_uint op1_len, const char *op2, zend_uint op2_len, int self_var TSRMLS_DC);
 void zephir_concat_sv(zval **result, const char *op1, zend_uint op1_len, zval *op2, int self_var TSRMLS_DC);
 void zephir_concat_svs(zval **result, const char *op1, zend_uint op1_len, zval *op2, const char *op3, zend_uint op3_len, int self_var TSRMLS_DC);
 void zephir_concat_vs(zval **result, zval *op1, const char *op2, zend_uint op2_len, int self_var TSRMLS_DC);
+void zephir_concat_vsvsv(zval **result, zval *op1, const char *op2, zend_uint op2_len, zval *op3, const char *op4, zend_uint op4_len, zval *op5, int self_var TSRMLS_DC);
 void zephir_concat_vv(zval **result, zval *op1, zval *op2, int self_var TSRMLS_DC);
 void zephir_concat_function(zval *result, zval *op1, zval *op2 TSRMLS_DC);
