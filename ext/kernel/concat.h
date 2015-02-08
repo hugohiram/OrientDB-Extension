8b3c8174f7e72ea6eac796c5ad9082a3203c40f7
hugohiram <hugo@hugohiram.com>
2015-02-08 04:28:03 +0100

    included some changes to the c libraries

--- ext/kernel/concat.h
+++ ext/kernel/concat.h
@@ -28,6 +28,11 @@
 #define ZEPHIR_SCONCAT_VV(result, op1, op2) \
 	 zephir_concat_vv(&result, op1, op2, 1 TSRMLS_CC);
 
+#define ZEPHIR_CONCAT_VVVVVVVV(result, op1, op2, op3, op4, op5, op6, op7, op8) \
+	 zephir_concat_vvvvvvvv(&result, op1, op2, op3, op4, op5, op6, op7, op8, 0 TSRMLS_CC);
+#define ZEPHIR_SCONCAT_VVVVVVVV(result, op1, op2, op3, op4, op5, op6, op7, op8) \
+	 zephir_concat_vvvvvvvv(&result, op1, op2, op3, op4, op5, op6, op7, op8, 1 TSRMLS_CC);
+
 
 void zephir_concat_sv(zval **result, const char *op1, zend_uint op1_len, zval *op2, int self_var TSRMLS_DC);
 void zephir_concat_svs(zval **result, const char *op1, zend_uint op1_len, zval *op2, const char *op3, zend_uint op3_len, int self_var TSRMLS_DC);
@@ -35,4 +40,5 @@ void zephir_concat_svsv(zval **result, const char *op1, zend_uint op1_len, zval 
 void zephir_concat_vs(zval **result, zval *op1, const char *op2, zend_uint op2_len, int self_var TSRMLS_DC);
 void zephir_concat_vsvsv(zval **result, zval *op1, const char *op2, zend_uint op2_len, zval *op3, const char *op4, zend_uint op4_len, zval *op5, int self_var TSRMLS_DC);
 void zephir_concat_vv(zval **result, zval *op1, zval *op2, int self_var TSRMLS_DC);
+void zephir_concat_vvvvvvvv(zval **result, zval *op1, zval *op2, zval *op3, zval *op4, zval *op5, zval *op6, zval *op7, zval *op8, int self_var TSRMLS_DC);
 void zephir_concat_function(zval *result, zval *op1, zval *op2 TSRMLS_DC);
